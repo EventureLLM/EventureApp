1ca33d58997317f61e6c2c8f30b1c14d5449bb59
jere67 <jeremymoon567@gmail.com>
2025-04-15 00:36:33 -0400

    fullstack modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [events, setEvents] = useState('');
+
+  const getEventsQuery = () => {
+    fetch('http://127.0.0.1:5000/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not successful.');
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+        console.log(data.response)
+        setEvents(data.response);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  };
 
   return (
     <>
@@ -17,6 +36,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>{events}</p>
+      <button onClick={getEventsQuery}>Get Events</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
